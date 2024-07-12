467ea850315e705ca324899607d20e4da4664dab
Emily Alvim <105649387+snoozleEmily@users.noreply.github.com>
2024-07-12 11:21:24 -0300

    Added Custom Font

--- src/market_board.py
+++ src/market_board.py
@@ -5,14 +5,15 @@ from scenes import *
 with open(r'Python-studies\shop_shop_manager\items\all_items.csv', 'r') as file:
     all_items = np.genfromtxt(file, delimiter=',', dtype=str)
 
-FONT = pygame.font.Font(None, 24)
+FONT = pygame.font.Font(r'Python-studies\shop_shop_manager\assets\SKEETCH_.TTF', 36)
 CELL_WIDTH, CELL_HEIGHT = 105, 25
 PADDING = 12
+ROW_MARGIN = 9
 
 table_width = Globals.screen_width - PADDING
 table_height = (Globals.screen_height // 2) - (PADDING * 5)
 
-rows_per_page = table_height // CELL_HEIGHT
+rows_per_page = (table_height - CELL_HEIGHT - ROW_MARGIN) // (CELL_HEIGHT + ROW_MARGIN)
 cols_per_page = table_width // CELL_WIDTH
 
 # Pagination
@@ -29,19 +30,17 @@ def render_table(display_surface):
 
     for i, row in enumerate(all_items[start_row:end_row]):
         for j, cell in enumerate(row):
-            if i == 0: # Titles
+            if i == 0: # Column Titles
                 text_surface = FONT.render(cell, True, (0, 0, 0))
                 x = j * CELL_WIDTH + PADDING
-                y = PADDING
+                y = PADDING / 3
                 viewport_surface.blit(text_surface, (x, y))
             else: 
                 text_surface = FONT.render(cell, True, (0, 0, 0))
                 x = j * CELL_WIDTH + PADDING
-                y = i * CELL_HEIGHT + PADDING
+                y = i * (CELL_HEIGHT + ROW_MARGIN) + PADDING
                 viewport_surface.blit(text_surface, (x, y))
 
     display_surface.blit(viewport_surface, (PADDING, PADDING))
-    
-
     pygame.display.flip()
 
