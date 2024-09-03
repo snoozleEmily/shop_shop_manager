3a4cc3fe39366918dca7e69c43630a6401c7f288
snoozleEmily <emilyalvimicm@gmail.com>
2024-09-03 20:52:05 -0300

    Update dice.py

--- src/scenes/tavern/dice.py
+++ src/scenes/tavern/dice.py
@@ -9,12 +9,21 @@ class Dice:
     die_face = None
     minigame_active = False
 
-    # The cup with the dice is in Button class
+    # The rolling cup (with the dice in it) is in the Button class
     def __init__(self, n=1):
         self.dice_images = {
-            i: (DICE_IMG_PATH + f"{i}.png", load_image(DICE_IMG_PATH + f"{i}.png"))
+            # Iterate through numbers 1 to 6 (representing dice faces)
+            i: (
+                # Construct the file path for the dice face image
+                DICE_IMG_PATH + f"{i}.png",
+                # Load the image from the file path
+                load_image(DICE_IMG_PATH + f"{i}.png"),
+            )
+            # Create a dictionary entry for each dice face number
             for i in range(1, 7)
         }
+
+        # Set the default image, using 'n' or face '1' if 'n' is invalid
         self.default_image = self.dice_images.get(n, self.dice_images[1])[1]
         self.current_image = self.default_image
 
@@ -22,4 +31,5 @@ class Dice:
         """Rolls the dice, updates the current face image, and returns its path."""
         face_number = random.randint(1, 6)
         self.current_image = self.dice_images[face_number][1]
-        return self.dice_images[face_number][0]
+        new_face_path = self.dice_images[face_number][0]
+        return new_face_path
